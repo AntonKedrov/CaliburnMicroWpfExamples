bf24732171c21bc1e05dd13636c83eac2fe54dd4
Anton <a-k@gmx.com>
2014-11-09 20:24:51 +0200

    DialogExample

--- CaliburnMicroWpfExamples/ShellViewModel.cs
+++ CaliburnMicroWpfExamples/ShellViewModel.cs
@@ -4,5 +4,36 @@ namespace CaliburnMicroWpfExamples
 {
     public class ShellViewModel : PropertyChangedBase, IShell
     {
+        private readonly IWindowManager _windowManager;
+
+        private string _dialogResult;
+
+        public string DialogResult
+        {
+            get
+            {
+                return _dialogResult;
+            }
+            set
+            {
+                _dialogResult = value;
+                NotifyOfPropertyChange(() => DialogResult);
+            }
+        }
+
+        public ShellViewModel(IWindowManager windowManager)
+        {
+            _windowManager = windowManager;
+        }
+
+        public void DialogExample()
+        {
+            bool? result = _windowManager.ShowDialog(new DialogExample.MainViewModel());
+
+            if (result.HasValue)
+                DialogResult = result.Value.ToString();
+            else
+                DialogResult = "NULL";
+        }
     }
 }
