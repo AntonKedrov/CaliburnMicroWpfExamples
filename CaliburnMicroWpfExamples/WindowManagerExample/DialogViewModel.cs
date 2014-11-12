5c2973b202500edb8e28e2e501d3646bf67e5fde
Anton <a-k@gmx.com>
2014-11-12 20:16:42 +0200

    Lost ViewModel in prev. commit

---
+++ CaliburnMicroWpfExamples/WindowManagerExample/DialogViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliburnMicroWpfExamples.WindowManagerExample
+{
+    class DialogViewModel : Screen
+    {
+        public void OK()
+        {
+            try
+            {
+                TryClose(true);
+            }
+            catch (Exception ex)
+            {
+                HandleInvalidOperationException(ex);
+            }
+        }
+
+        public void Cancel()
+        {
+            try
+            {
+                TryClose(false);
+            }
+            catch (Exception ex)
+            {
+                HandleInvalidOperationException(ex);
+            }
+        }
+
+        private void HandleInvalidOperationException(Exception ex)
+        {
+            if (ex.InnerException is InvalidOperationException)
+                throw ex.InnerException;
+            else
+                throw ex;
+        }
+    }
+}
